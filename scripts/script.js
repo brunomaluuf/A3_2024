c70544c0c13232fdfda2e443597c499f75889c43
brunomaluuf <104863574+brunomaluuf@users.noreply.github.com>
2024-05-26 03:18:13 -0300

    Novas telas e quase finalizadas as existentes
    Feitas alterações nas telas de menu, criada a aba de cadastro de aluno para que o admin quando estiver logado, tenha, dentro do sistema, acesso a uma tela de cadastro.

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const clockElement = document.getElementById('clock');
     const menuIcon = document.getElementById('menu-icone');
     const menu = document.getElementById('menu');
+    const cpfInput = document.getElementById('cpf');
+    const rgInput = document.getElementById('rg');
 
     menuIcon.addEventListener('click', () => {
         menu.classList.toggle('open');
@@ -24,4 +26,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
     fetchTime();
     setInterval(fetchTime, 1000);
+
+    cpfInput.addEventListener('input', () => {
+        let value = cpfInput.value.replace(/\D/g, ''); 
+        if (value.length > 11) value = value.slice(0, 11); 
+
+        if (value.length <= 11) {
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d{1,2})$/, '$1-$2');
+        }
+        cpfInput.value = value;
+    });
+
+    rgInput.addEventListener('input', () => {
+        let value = rgInput.value.replace(/\D/g, ''); 
+        if (value.length > 9) value = value.slice(0, 9); 
+
+        if (value.length <= 9) {
+            value = value.replace(/(\d{2})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d)/, '$1.$2');
+            value = value.replace(/(\d{3})(\d{1})$/, '$1-$2');
+        }
+        rgInput.value = value;
+    });
 });
+
+
+/*function cadastrar(){
+    alert("Cadastro realizado com sucesso.")
+
+    VERIFICAR COMO FAZER O ALERTA APARECER SÓ DPS DE PREENCHER TUDO.
+}*/ 
